1835b847ed89e892c25a98408779b60f7d9393f3
Rahul-krishh <rahulmykris@gmail.com>
2026-01-16 22:02:33 +0530

    feat : load date from backend, and add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Routes , Route } from 'react-router-dom'
 import { HomePage } from './assets/pages/HomePage'
-import { CheckoutPage } from './assets/pages/CheckoutPage'
+import { CheckoutPage } from './assets/pages/checkout/CheckoutPage'
 import { OrderPage } from './assets/pages/OrderPage'
 import { TrackingPage } from './assets/pages/TrackingPage'
+import { NotFoundPage } from './assets/pages/NotFoundPage'
 import './App.css'
 
 function App() {
@@ -10,10 +11,11 @@ function App() {
     <>
       <Routes>
         {/* <Route path='/' element={ <HomePage/> }></Route> */}
-        <Route index element={ <HomePage /> }></Route>
-        <Route path='checkout' element={ <CheckoutPage/> }></Route>
-        <Route path='orders' element={ <OrderPage/> }></Route>
-        <Route path='track' element={ <TrackingPage/> }></Route>
+        <Route index element={ <HomePage /> }/>
+        <Route path='checkout' element={ <CheckoutPage/> }/>
+        <Route path='orders' element={ <OrderPage/> }/>
+        <Route path='track' element={ <TrackingPage/> }/>
+        <Route path='*' element={ <NotFoundPage/> }/>
       </Routes>
     </>
   )
---
+++ src/assets/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header"
+import './NotFoundPage.css'
+export function NotFoundPage () {
+  return (
+    <div className="error-cont">
+      <title>QuickCart</title>
+      <Header/>
+      <h2>404. That&apos;s an error.</h2>
+      <div>The requested ws not found on this server right now.</div>
+    </div>
+  )
+};
